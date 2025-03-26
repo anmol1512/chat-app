f3f92687887f7a4335f0e617ca282493d4b0d10f
anmol chhetri <anmol1512@gmail.com>
2025-03-27 02:48:25 +0530

    #3 | Added routes for message and created schema for message 1. added getAllUser, getMessage, sendMessage routes. 2. Created Message schema containing: senderId, receiverId, text and image.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js"
 
 const app = express();
 
 app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
 
 app.listen(5001, () => {
     console.log("server is running on 5001");
